c089182222bda8b9a95dc727bdde7f1fee258223
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-02-29 21:02:11 -0300

    ✨ feat: repositório frontend inicializado
    - Adicionado no front-end as bibliotecas e arquivos iniciais apenas para testar se as libs foram corretamente instaladas

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+  <div class="shrink-0">
+
+  </div>
+  <div>
+    <div class="text-xl font-medium text-black">Instalações</div>
+    <p class="text-slate-500">Tailwind e Vue instalados com sucesso</p>
+  </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
