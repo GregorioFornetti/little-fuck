27fe5d4b5b16cb1e0eefad907033f2c17195e861
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-03-17 17:18:58 -0300

    ✨ feat: lidando com erro de conexão ao servidor

--- frontend/src/global.ts
+++ frontend/src/global.ts
@@ -1,5 +1,11 @@
 
 import type Lobby from "./interfaces/Lobby"
 import type { Ref } from "vue"
+import type { Socket } from "socket.io-client"
 
-export var lobby: Ref<Lobby> | undefined
+declare global {
+    /** Variável global que armazena informações do lobby que o usuário está. Será undefined se o usuário não estiver em nenhum lobby */
+    var lobby: Ref<Lobby>
+    /** Variável global que armazena o socket de comunicação com o servidor. Será undefined se o usuário não estiver conectado */
+    var socket: Socket
+}
