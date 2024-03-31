ca10a1db91e7f7f0509979a396d4885f1414c550
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-03-31 12:31:31 -0300

    ✅ test: testes handleCreateLobby finalizados

--- backend/src/__tests__/lobby/testHandleCreateLobby.ts
+++ backend/src/__tests__/lobby/testHandleCreateLobby.ts
@@ -7,6 +7,7 @@ import { lobbys, players } from "../../global";
 import { handleCreateLobby } from "../../events/lobby/eventsHandlers";
 import Player from "../../interfaces/Player";
 import EventsEmitter from "../../events/Emitter";
+import Lobby from "../../interfaces/Lobby";
 
 
 function waitFor(socket: ServerSocket | ClientSocket, event: string) {
@@ -37,13 +38,16 @@ describe("handleCreateLobby", () => {
       });
       clientSocket.on("connect", done);
     });
+  });
 
+  beforeEach(() => {
     for (let lobby in lobbys) {
       delete lobbys[lobby]
     }
     for (let player in players) {
       delete players[player]
     }
+    clientSocket.removeAllListeners()
   });
 
   afterAll(() => {
@@ -56,9 +60,61 @@ describe("handleCreateLobby", () => {
   test("Nome vazio deve emitir erro", (done) => {
     clientSocket.on('join-lobby-error', (errorType: string) => {
       expect(errorType).toBe('no-name');
+      expect(lobbys).toEqual({});
+      expect(players).toEqual({});
       done();
     })
 
     handleCreateLobby(player, '');
+  });
+
+  test("Nome apenas com espaços deve emitir erro", (done) => {
+    clientSocket.on('join-lobby-error', (errorType: string) => {
+      expect(errorType).toBe('no-name');
+      expect(lobbys).toEqual({});
+      expect(players).toEqual({});
+      done();
+    })
+
+    handleCreateLobby(player, '       ');
+  });
+
+  test("Criar lobby com nome válido", (done) => {
+    clientSocket.on('join-lobby-success', (lobby: Lobby) => {
+      console.log('oi2')
+      // Verifica se as informações do lobby foram enviadas corretamente para o jogador
+      expect(lobby.lobbyId).toBeDefined();
+      expect(lobby.players).toHaveLength(1);
+      expect(lobby.players[0].id).toBe(player.playerId);
+      expect(lobby.players[0].name).toBe('player1');
+      expect(lobby.players[0].leader).toBe(true);
+      expect(lobby.players[0].ready).toBe(false);
+      expect(lobby.game).toBeUndefined();
+
+      // Verifica se as informações do lobby foram salvas corretamente no servidor
+      expect(lobbys[lobby.lobbyId]).toEqual(lobby);
+      expect(players[player.playerId]).toEqual(lobby);
+
+      done();
+    })
+
+    handleCreateLobby(player, 'player1');
+  });
+
+  test("Criar lobby com jogador já em outro lobby deve emitir erro", (done) => {
+
+    // A primeira vez chamando handleCreateLobby deve ser bem sucedida
+    clientSocket.on('join-lobby-success', (lobby: Lobby) => {
+      player.lobby = lobby;
+      handleCreateLobby(player, 'player1');
+    })
+
+    // A segunda vez chamando handleCreateLobby deve emitir erro
+    clientSocket.on('join-lobby-error', (errorType: string) => {
+      expect(errorType).toBe('player-already-in-lobby');
+      done();
+    })
+
+    handleCreateLobby(player, 'player1');
   });
 });
