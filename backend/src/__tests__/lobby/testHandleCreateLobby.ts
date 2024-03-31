26422eca9dbbba81723540215f3eba8cbf743164
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-03-31 13:00:13 -0300

    ✅ test: setupFile

--- backend/src/__tests__/lobby/testHandleCreateLobby.ts
+++ backend/src/__tests__/lobby/testHandleCreateLobby.ts
@@ -1,62 +1,13 @@
 
-import { createServer } from "node:http";
-import { AddressInfo } from "net";
-import { io as ioc, Socket as ClientSocket } from "socket.io-client";
-import { Server, Socket as ServerSocket } from "socket.io";
 import { lobbys, players } from "../../global";
 import { handleCreateLobby } from "../../events/lobby/eventsHandlers";
-import Player from "../../interfaces/Player";
-import EventsEmitter from "../../events/Emitter";
 import Lobby from "../../interfaces/Lobby";
 
-
-function waitFor(socket: ServerSocket | ClientSocket, event: string) {
-  return new Promise((resolve) => {
-    socket.once(event, resolve);
-  });
-}
+import { clientSocket, player } from "../setupTests";
 
 
 describe("handleCreateLobby", () => {
-  let io: Server
-  let clientSocket: ClientSocket
-  let serverSocket: ServerSocket
-  let player: Player
 
-  beforeAll((done) => {
-    const httpServer = createServer();
-    io = new Server(httpServer);
-    httpServer.listen(() => {
-      const port = (httpServer.address() as AddressInfo).port;
-      clientSocket = ioc(`http://localhost:${port}`);
-      io.on("connection", (socket) => {
-        serverSocket = socket;
-        player = {
-          playerId: clientSocket.id as string,
-          eventsEmitter: new EventsEmitter(io, serverSocket),
-        }
-      });
-      clientSocket.on("connect", done);
-    });
-  });
-
-  beforeEach(() => {
-    for (let lobby in lobbys) {
-      delete lobbys[lobby]
-    }
-    for (let player in players) {
-      delete players[player]
-    }
-    clientSocket.removeAllListeners()
-  });
-
-  afterAll(() => {
-    io.close();
-    clientSocket.disconnect();
-  });
-
-
-  
   test("Nome vazio deve emitir erro", (done) => {
     clientSocket.on('join-lobby-error', (errorType: string) => {
       expect(errorType).toBe('no-name');
@@ -81,7 +32,6 @@ describe("handleCreateLobby", () => {
 
   test("Criar lobby com nome válido", (done) => {
     clientSocket.on('join-lobby-success', (lobby: Lobby) => {
-      console.log('oi2')
       // Verifica se as informações do lobby foram enviadas corretamente para o jogador
       expect(lobby.lobbyId).toBeDefined();
       expect(lobby.players).toHaveLength(1);
---
+++ backend/src/__tests__/setupTests.ts
@@ -0,0 +1,47 @@
+
+import { createServer } from "node:http";
+import { AddressInfo } from "net";
+import { io as ioc, Socket as ClientSocket } from "socket.io-client";
+import { Server, Socket as ServerSocket } from "socket.io";
+import { lobbys, players } from "../global";
+import Player from "../interfaces/Player";
+import EventsEmitter from "../events/Emitter";
+
+const httpServer = createServer();
+let io: Server;
+let clientSocket: ClientSocket;
+let serverSocket: ServerSocket;
+let player: Player;
+
+beforeAll((done) => {
+  io = new Server(httpServer);
+  httpServer.listen(() => {
+    const port = (httpServer.address() as AddressInfo).port;
+    clientSocket = ioc(`http://localhost:${port}`);
+    io.on("connection", (socket) => {
+      serverSocket = socket;
+      player = {
+        playerId: clientSocket.id as string,
+        eventsEmitter: new EventsEmitter(io, serverSocket),
+      };
+    });
+    clientSocket.on("connect", done);
+  });
+});
+
+afterEach(() => {
+  for (let lobby in lobbys) {
+    delete lobbys[lobby];
+  }
+  for (let player in players) {
+    delete players[player];
+  }
+  clientSocket.removeAllListeners();
+});
+
+afterAll(() => {
+  io.close();
+  clientSocket.disconnect();
+});
+
+export { io, clientSocket, serverSocket, player };
