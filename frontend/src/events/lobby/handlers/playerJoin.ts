1abd72cd0ee4624f80d47c31eeff667a0dc6e657
lauanS <lauan.s.souza@gmail.com>
2024-05-11 15:40:39 -0300

    :sparkles: feat: Implement playerJoin event handler

--- frontend/src/events/lobby/handlers/playerJoin.ts
+++ frontend/src/events/lobby/handlers/playerJoin.ts
@@ -9,5 +9,18 @@ import { lobby } from '@/connection'
  *  @param name nome do jogador que acabou de entrar na sala
  */
 export function handlePlayerJoin(id: string, name: string) {
+  if (lobby.value === null) {
+    throw new Error('Você não está em um lobby !');
+  }
 
+  if (lobby.value.game) {
+    throw new Error('Não foi possível adicionar um novo jogador ao seu lobby atual, o jogo já começou !');
+  }
+
+  lobby.value.players.push({
+    id,
+    name,
+    leader: false,
+    ready: false
+  });
 }
