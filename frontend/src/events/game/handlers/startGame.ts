a9872cf010558e4af9ae54185d3249a958588f91
lauanS <lauan.s.souza@gmail.com>
2024-05-14 18:15:36 -0300

    👔 feat: Simplify frontend logic by removing specific verifications in startGame

--- frontend/src/events/game/handlers/startGame.ts
+++ frontend/src/events/game/handlers/startGame.ts
@@ -7,8 +7,6 @@ const INITIAL_WAIT_TIME = 5;
 const INITIAL_MATCH_NUMBER = 1;
 const INITIAL_ROUND_NUMBER = 1;
 
-const MIN_PLAYERS_REQUIRED = 2;
-
 /**
  *  Evento indicando o início de um jogo completo de "Little Fuck"
  */
@@ -19,15 +17,6 @@ export function handleStartGame() {
 
   if (lobby.value.game) {
     throw new Error('Não foi possível iniciar o jogo: O jogo já começou !');
-  }
-
-  if (lobby.value.players.length < MIN_PLAYERS_REQUIRED) {
-    throw new Error('Não foi possível iniciar o jogo: Número de jogadores insuficiente ! (min: 2)');
-  }
-
-  const hasLeader = lobby.value.players.find((player) => player.leader)
-  if (!hasLeader) {
-    throw new Error('Não foi possível iniciar o jogo: Nenhum jogador é o líder do lobby');
   }
 
   const players = lobby.value.players as Array<{ id: string }>;
