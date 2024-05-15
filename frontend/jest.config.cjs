22d2d100803a8ab76f7c55539a9c400ee4ca4e3e
lauanS <lauan.s.souza@gmail.com>
2024-05-15 13:51:54 -0300

    🔧 chore: Configure Jest to ignore files starting with _ (underline)

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -3,7 +3,8 @@ module.exports = {
     testEnvironment: 'node',
     testMatch: [
         "**/__tests__/**/*",
-        "!**/__tests__/**/setupTests.[jt]s?(x)"
+        "!**/__tests__/**/setupTests.[jt]s?(x)",
+        "!**/__tests__/**/_*"
     ],
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1', // This maps @ to src directory
