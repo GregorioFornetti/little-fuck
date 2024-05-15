c521bc7be4e1d7959a3cb2f924a4f7a41b7eae78
lauanS <lauan.s.souza@gmail.com>
2024-05-15 14:13:54 -0300

    🔇 chore: Remove i18n missing warnings for test environment

--- frontend/src/plugins/i18n.ts
+++ frontend/src/plugins/i18n.ts
@@ -3,9 +3,13 @@ import ptBR from '@/locales/pt-BR.json';
 
 type MessageSchema  = typeof ptBR;
 
+const environment = process.env.NODE_ENV
+
 const i18n = createI18n<[MessageSchema], 'pt-BR'>({
   // For use with Vue Composition API
   legacy: false,
+  // Hide warnings only for test environment
+  missingWarn: environment !== 'test',
   locale: 'pt-BR',
   messages: {
     'pt-BR': ptBR
