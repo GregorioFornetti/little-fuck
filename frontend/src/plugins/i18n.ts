ee48abc9c430fc83a49d1bf5a8103c6a451cd47c
lauanS <lauan.s.souza@gmail.com>
2024-05-15 12:32:09 -0300

    🔧 chore: Enable Vue Composition API with i18n support

--- frontend/src/plugins/i18n.ts
+++ frontend/src/plugins/i18n.ts
@@ -4,6 +4,8 @@ import ptBR from '@/locales/pt-BR.json';
 type MessageSchema  = typeof ptBR;
 
 const i18n = createI18n<[MessageSchema], 'pt-BR'>({
+  // For use with Vue Composition API
+  legacy: false,
   locale: 'pt-BR',
   messages: {
     'pt-BR': ptBR
