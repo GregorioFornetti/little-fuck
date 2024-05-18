b5ae125d7edda587913b67bf525c57582c0cf2bd
lauanS <lauan.s.souza@gmail.com>
2024-05-18 11:36:15 -0300

    🔧 chore: Update jest.config to support .test sub-extension

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -2,9 +2,7 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: [
-        "**/__tests__/**/*",
-        "!**/__tests__/**/setupTests.[jt]s?(x)",
-        "!**/__tests__/**/_*"
+        "**/__tests__/**/*.test.[jt]s?(x)",
     ],
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1', // This maps @ to src directory
