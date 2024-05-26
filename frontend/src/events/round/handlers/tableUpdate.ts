61454f381c91ccf98453f55135ec489da302f688
lauanS <lauan.s.souza@gmail.com>
2024-05-26 14:53:45 -0300

    ✨ feat: Implement tableUpdate event handler

--- frontend/src/events/round/handlers/tableUpdate.ts
+++ frontend/src/events/round/handlers/tableUpdate.ts
@@ -1,7 +1,6 @@
-
 import type { RoundCards } from "@/interfaces/Lobby";
-import { lobby } from '@/connection'
-
+import { lobby } from '@/connection';
+import { i18n } from "@/plugins/i18n";
 
 /**
  *  Após um jogador selecionar uma carta, essa mensagem será enviada para que todos clientes da sala atualizem
@@ -11,5 +10,24 @@ import { lobby } from '@/connection'
  *  @param nextPlayerId id do próximo jogador que deve jogar. Pode ser undefined caso todos os jogadores já tenham jogado
  */
 export function handleTableUpdate(cards: RoundCards, nextPlayerId?: string) {
+  if (lobby.value === null) {
+    throw new Error(i18n.t('COMMON.ERROR.NOT_IN_LOBBY'));
+  }
 
+  if (!lobby.value.game) {
+    throw new Error(i18n.t('COMMON.ERROR.GAME_NOT_STARTED'));
+  }
+
+  if (!lobby.value.game.match) {
+    throw new Error(i18n.t('COMMON.ERROR.MATCH_NOT_STARTED'));
+  }
+
+  if (!lobby.value.game.match.round) {
+    throw new Error(i18n.t('COMMON.ERROR.ROUND_NOT_STARTED'));
+  }
+
+  const round = lobby.value.game.match.round;
+
+  round.cards = cards;
+  round.nextPlayerId = nextPlayerId;
 }
