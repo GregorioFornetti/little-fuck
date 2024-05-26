df19135189aae694e06341bae84b9c107bb858dd
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-05-26 17:01:25 -0300

    ✨ feat: Implement selectRandomIndex function

---
+++ backend/src/functions/selectRandomIndex.ts
@@ -0,0 +1,13 @@
+
+/**
+ *  Seleciona um índice aleatório de um array
+ *
+ *  @param array uma lista qualquer, a qual será selecionado um índice aleatório
+ *  @returns um índice aleatório do array
+ */
+export function selectRandomIndex<T>(array: T[]): number {
+    if (array.length === 0) {
+        throw new Error("Cannot select a random index from an empty array");
+    }
+    return Math.floor(Math.random() * array.length);
+}
