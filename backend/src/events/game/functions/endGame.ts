14201fee1ac0f3608945d04bdda5c26ccfe3ce5a
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-05-30 10:49:37 -0300

    🔧 chore: Create endGame signature

---
+++ backend/src/events/game/functions/endGame.ts
@@ -0,0 +1,11 @@
+
+/**
+ *  Função que finaliza uma jogo de "little-fuck". Essa função é responsável por:
+ *  - Desenstanciar o Game
+ *  - Enviar mensagens para todos os jogadores que o jogo finalizou
+ *  
+ *  @param player um jogador qualquer que está na partida
+ */
+export function endGame(player: Player): void {
+
+}
