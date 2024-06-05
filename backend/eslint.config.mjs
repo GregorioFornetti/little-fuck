19334161029dbfeac3d0b933f5250efd1dd6e6fd
lauanS <lauan.s.souza@gmail.com>
2024-06-05 14:38:12 -0300

    🔧 chore: Add custom rules for ESLint

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,9 +1,32 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+      'indent': ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      'capitalized-comments': ['warn'],
+      'eol-last': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'require-atomic-updates': 'off',
+      'no-console': 'off',
+      'no-trailing-spaces': ['error'],
+      'no-multi-spaces': ['error'],
+      'no-multiple-empty-lines': ['error', { 'max': 1 }],
+      'comma-spacing': ['error'],
+      'arrow-spacing': ['error'],
+      'space-infix-ops': ['error'],
+      'block-spacing': ['error'],
+      'object-curly-spacing': ['error', 'always'],
+      'spaced-comment': ['error', 'always'],
+      'space-before-blocks': ['error', 'always'],
+    }
+  }
 ];
