faf9ce4c74c58cb801ebe32664534984a0d1d2c2
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-06-12 23:54:26 -0300

    🚨 chore: Remove unused event emitter
    - Como é preciso emitir o evento de debug em um nível mais baixo (como pode ser visto na função addDebugEmit), essse emitter se tornou inútil

--- backend/src/events/general/eventsEmitter.ts
+++ backend/src/events/general/eventsEmitter.ts
@@ -37,17 +37,4 @@ export default class GeneralEventsEmitter extends EmitterBase {
   public emitInternalServerError() {
     this.emitToLobby('internal-server-error');
   }
-
-  /**
-   *  Evento que é enviado após cada modificação ocorrida no `backend`. 
-   *  Esse evento irá transmitir o estado das informações de forma "cru" para o frontend, para fins de debug.
-   *  Este evento só sera acionado quando o servidor for executado em modo de debug (através do comando `npm run debug`).
-   */
-  public emitDebug(lobby: Lobby) {
-    if (!process.env.DEBUG) {
-      throw new Error(i18n.t('COMMON.ERROR.NO_DEBUG_MODE'))
-    }
-    this.emitToLobby('debug', lobby);
-    // this.emitToLobby('debug', lobby);
-  }
 }
