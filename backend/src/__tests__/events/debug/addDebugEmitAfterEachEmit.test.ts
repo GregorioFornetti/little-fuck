ec2a1dc3d7ee8768768102542c04463a43f25c6e
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-06-13 09:31:37 -0300

    ✅ test: Update addDebugEmit tests

--- backend/src/__tests__/events/debug/addDebugEmitAfterEachEmit.test.ts
+++ backend/src/__tests__/events/debug/addDebugEmitAfterEachEmit.test.ts
@@ -12,18 +12,21 @@ let io: Server;
 let clientSocket: ClientSocket;
 let serverSocket: ServerSocket;
 
-beforeAll((done) => {
+function instancianteSockets(onDone: () => void, useAddDebug: boolean) {
   io = new Server(httpServer);
-  addDebugEmitAfterEachEmit(io)
+  if (useAddDebug) {
+    addDebugEmitAfterEachEmit(io)  // Essa função adiciona um middleware que emite um evento 'debug' após cada evento emitido. Por isso precisa ser chamada antes de instanciar os sockets.
+  }
   httpServer.listen(() => {
     const port = (httpServer.address() as AddressInfo).port;
     clientSocket = ioc(`http://localhost:${port}`);
     io.on('connection', (socket) => {
       serverSocket = socket;
+      onDone()
     });
-    clientSocket.on('connect', () => done());
+    clientSocket.on('connect', () => {});
   });
-});
+}
 
 afterEach(() => {
   for (const lobby in lobbys) {
@@ -34,9 +37,7 @@ afterEach(() => {
   }
   clientSocket.removeAllListeners();
   clientSocket.removeAllListeners('player-join');
-});
 
-afterAll(() => {
   httpServer.close();
   io.close();
   clientSocket.disconnect();
@@ -44,33 +45,54 @@ afterAll(() => {
 
 describe('addDebugEmitAfterEachEmit', () => {
 
-    test('Deve emitir evento debug após cada evento emitido', (done) => {
+  test('Deve emitir evento debug após cada evento emitido', (done) => {
+    instancianteSockets(() => {
+      players[serverSocket.id] = {
+        socket: serverSocket
+      }
 
-        players[serverSocket.id] = {
-            socket: serverSocket
+      serverSocket.emit('test')
+      clientSocket.on('debug', (lobby: any) => {
+        done()
+      })
+    
+    }, true)
+  })
+
+  test('Deve emitir o evento de debug com o lobby correto', (done) => {
+        
+    instancianteSockets(() => {
+      players[serverSocket.id] = {
+        socket: serverSocket,
+        lobby: {
+            lobbyId: '123',
+            players: []
         }
+      }
 
-        serverSocket.emit('test')
-        clientSocket.on('debug', (lobby: any) => {
-            done()
-        })
-    })
+      serverSocket.emit('test')
+      clientSocket.on('debug', (lobby: any) => {
+        expect(lobby.lobbyId).toBe('123')
+        expect(lobby.players).toStrictEqual([])
+        done()
+      })
+    }, true)
+  })
 
-    test('Deve emitir o evento de debug com o lobby correto', (done) => {
-            
-        players[serverSocket.id] = {
-            socket: serverSocket,
-            lobby: {
-                lobbyId: '123',
-                players: []
-            }
-        }
+  test('Não deve emitir o evento de debug se não for cadastrado o middleware', (done) => {
+    instancianteSockets(() => {
+      players[serverSocket.id] = {
+        socket: serverSocket
+      }
 
-        serverSocket.emit('test')
-        clientSocket.on('debug', (lobby: any) => {
-            expect(lobby.lobbyId).toBe('123')
-            expect(lobby.players).toStrictEqual([])
-            done()
-        })
-    })
+      serverSocket.emit('test')
+      clientSocket.on('debug', (lobby: any) => {
+        fail('Não deveria ter emitido o evento debug')
+      })
+      
+      setTimeout(() => {
+        done()
+      }, 1000)
+    }, false)
+  })
 })
