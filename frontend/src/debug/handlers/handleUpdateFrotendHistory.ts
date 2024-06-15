36f53ac2a81758a9550ab83a565874e87c4fef94
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-06-14 23:10:33 -0300

    ✨ feat: Create handleUpdateFrontendHistory func

---
+++ frontend/src/debug/handlers/handleUpdateFrotendHistory.ts
@@ -0,0 +1,16 @@
+
+import { frontendLobbyHistoryList } from "../globals";
+import { lobby } from "@/connection";
+
+
+/**
+ *  Adiciona o lobby do cliente atual ao histórico (em `frontendLobbyHistoryList`). Não atualiza o histórico se o evento for `debug`.
+ *  
+ *  @param event nome do evento que foi chamado
+ *  @param args parâmetros deste evento
+ */
+export function handleUpdateFrontendHistory(event: string, ...args: any[]) {
+    if (event !== 'debug') {
+        frontendLobbyHistoryList.value = [...frontendLobbyHistoryList.value, lobby.value]
+    }
+}
