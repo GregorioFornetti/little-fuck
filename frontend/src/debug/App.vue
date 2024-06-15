cc42bd1ac2c8ceecc404de2153ff16b6cd687fda
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-06-14 23:14:01 -0300

    ✨ feat: Update debug App.vue to add new handlers

--- frontend/src/debug/App.vue
+++ frontend/src/debug/App.vue
@@ -1,4 +1,37 @@
+<script setup lang="ts">
+  import { socket, lobby } from "@/connection";
+  import { addHandlersForDebugMode } from "@/debug/handlers/addHandlersForDebugMode";
+
+  addHandlersForDebugMode(socket);
+
+  import { ref } from "vue";
+  import Spinner from "@/components/Spinner.vue";
+
+  const connectionStatus = ref<"loading" | "connected" | "disconnected">("loading")
+
+  socket.on("connect", () => {
+    console.log("connected")
+    connectionStatus.value = "connected"
+  });
+
+  socket.on("connect_error", (err) => {
+    console.log(`connect_error due to ${err.message}`);
+    connectionStatus.value = "disconnected"
+  });
+
+</script>
 
 <template>
-    <h1>DEBUG FUNCIONOU ?</h1>
+  <!-- Connecting -->
+  <div class="h-screen v-screen flex justify-center items-center" v-if="connectionStatus === 'loading'">
+    <Spinner :size="70" />
+  </div>
+  <!-- Connection error -->
+  <div class="h-screen v-screen flex justify-center items-center" v-if="connectionStatus === 'disconnected'">
+    <h1>Não foi possível se conectar ao servidor.<br>Tente novamente mais tarde</h1>
+  </div>
+  <!-- Connection success -->
+  <div v-if="connectionStatus === 'connected'">
+    <h1>conectou</h1>
+  </div>
 </template>
