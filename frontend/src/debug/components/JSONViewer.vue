15177db78d7a216c0757389c89e562f52fd2afb1
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-06-30 00:03:35 -0300

    🔧 chore: create JSONViewer signature

---
+++ frontend/src/debug/components/JSONViewer.vue
@@ -0,0 +1,12 @@
+
+<script setup lang="ts">
+
+// Remover comentário abaixo quando implementar o componente, juntamente com esse comentário atual
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const IDENT_SIZE = 2;
+
+defineProps<{
+  json: object
+}>();
+
+</script>
