64f57a46b56b3b51754aae0bb1a3af4621354f51
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-06-30 00:04:03 -0300

    🔧 chore: create EventsContainer signature

---
+++ frontend/src/debug/components/EventsContainer.vue
@@ -0,0 +1,10 @@
+
+<script setup lang="ts">
+// Remover comentário abaixo quando implementar o componente, juntamente com esse comentário atual
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import HistoricModal from './HistoricModal.vue';
+
+// Remover comentário abaixo quando implementar o componente, juntamente com esse comentário atual
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { eventsHistoryList } from '../globals';
+</script>
