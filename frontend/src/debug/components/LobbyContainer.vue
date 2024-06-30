d53a2ce3804d49493e3821f8b891cf60e64373d9
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-06-30 00:03:14 -0300

    🔧 chore: create LobbyContainer signature

---
+++ frontend/src/debug/components/LobbyContainer.vue
@@ -0,0 +1,15 @@
+
+<script setup lang="ts">
+// Remover comentário abaixo quando implementar o componente, juntamente com esse comentário atual
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import HistoricModal from './HistoricModal.vue';
+// Remover comentário abaixo quando implementar o componente, juntamente com esse comentário atual
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import JSONViewer from './JSONViewer.vue';
+
+defineProps<{
+  modalTitle: string,
+  jsonLabel: string,
+  jsonList: object[],
+}>();
+</script>
