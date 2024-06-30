de8cad9fd3116ec8b2a0bf60ad2f12c7ead19cca
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-06-30 00:03:49 -0300

    🔧 chore: create HistoricModal signature

---
+++ frontend/src/debug/components/HistoricModal.vue
@@ -0,0 +1,14 @@
+
+<script setup lang="ts">
+// Remover comentário abaixo quando implementar o componente, juntamente com esse comentário atual
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import BaseModal from '@/components/modal/BaseModal.vue';
+
+defineProps<{
+  title: string,
+  show: boolean,
+  jsonLabel: string,
+  jsonList: object[],
+  closeModal?: () => void,
+}>();
+</script>
