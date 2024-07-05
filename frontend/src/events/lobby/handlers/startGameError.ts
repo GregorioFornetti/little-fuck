08901e860012ba00431b89d35c659f4610197ec1
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-07-04 22:47:29 -0300

    ✨ feat: Implement startGameError handler

--- frontend/src/events/lobby/handlers/startGameError.ts
+++ frontend/src/events/lobby/handlers/startGameError.ts
@@ -1,4 +1,6 @@
 
+import { i18n } from '@/plugins/i18n';
+
 /**
  *  Evento indicando que ocorreu um erro ao iniciar a partida.
  *  Esse erro pode ocorrer caso o solicitante não esteja em um sala, ou que está já está em jogo.
@@ -6,7 +8,14 @@
  *
  *  @param type tipo de erro que ocorreu
  */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function handleStartGameError(type: 'not-leader'|'not-all-ready'|'not-in-lobby'|'already-in-game') {
-
+  if (type === 'not-leader') {
+    throw new Error(i18n.t('COMMON.ERROR.NOT_LEADER'));
+  } else if (type === 'not-all-ready') {
+    throw new Error(i18n.t('COMMON.ERROR.NOT_ALL_READY'));
+  } else if (type === 'not-in-lobby') {
+    throw new Error(i18n.t('COMMON.ERROR.NOT_IN_LOBBY'));
+  } else if (type === 'already-in-game') {
+    throw new Error(i18n.t('COMMON.ERROR.GAME_ALREADY_STARTED'));
+  }
 }
