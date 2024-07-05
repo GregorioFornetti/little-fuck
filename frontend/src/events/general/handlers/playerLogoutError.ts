29bc48c2b7f5007b1187eda940c80bbd30906a4c
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-07-04 22:44:56 -0300

    ✨ feat: Implement playerLogoutError handler

--- frontend/src/events/general/handlers/playerLogoutError.ts
+++ frontend/src/events/general/handlers/playerLogoutError.ts
@@ -1,10 +1,13 @@
 
+import { i18n } from '@/plugins/i18n';
+
 /**
  *  Evento indicando que ocorreu um erro ao sair de uma sala. Isso pode acontecer caso o cliente não estivesse em uma sala.
  *
  *  @param type tipo de erro que ocorreu
  */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function handlePlayerLogoutError(type: 'not-in-lobby') {
-
+  if (type === 'not-in-lobby') {
+    throw new Error(i18n.t('COMMON.ERROR.NOT_IN_LOBBY'));
+  }
 }
