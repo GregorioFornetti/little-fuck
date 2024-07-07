c2962de27101abbb5ca0f9b3c5720dc2bd4ca6f4
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-07-07 01:15:26 -0300

    ✨ feat: CommandsContainerInput component

---
+++ frontend/src/debug/components/ComandsContainer/CommandsContainerInput.vue
@@ -0,0 +1,20 @@
+
+<script setup lang="ts">
+defineProps<{
+  inputType: string,
+  placeholder: string
+}>();
+
+import { ref } from 'vue';
+
+const value = ref('');
+</script>
+
+<template>
+  <input
+    v-model="value"
+    class="w-full border-2 p-1 rounded-lg mb-3"
+    :placeholder="placeholder"
+    :type="inputType"
+  >
+</template>
