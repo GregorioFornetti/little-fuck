4bbe5c10bf6de8e6ed16940629984c1b99ba54b8
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-07-06 21:26:20 -0300

    ✨ feat: JSON Viewer component

--- frontend/src/debug/components/JSONViewer.vue
+++ frontend/src/debug/components/JSONViewer.vue
@@ -1,12 +1,36 @@
 
 <script setup lang="ts">
 
-// Remover comentário abaixo quando implementar o componente, juntamente com esse comentário atual
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const IDENT_SIZE = 2;
 
-defineProps<{
+const props = defineProps<{
   json: object
 }>();
 
+const jsonToDisplay = JSON.stringify(props.json, null, IDENT_SIZE);
 </script>
+
+<template>
+  <div class="w-full h-full overflow-auto rounded-lg bg-neutral-800 text-white scroll">
+    <pre>{{ jsonToDisplay }}</pre>
+  </div>
+</template>
+
+<style scoped>
+.scroll::-webkit-scrollbar {
+  width: 12px;
+  height: 12px;
+  /* move the scroll to inside the div */
+  margin-left: -12px;
+}
+
+.scroll::-webkit-scrollbar-track {
+  background: black;
+  border-radius: 0.5rem;
+}
+
+.scroll::-webkit-scrollbar-thumb {
+  background: rgb(245 245 244);
+  border-radius: 0.5rem;
+}
+</style>
