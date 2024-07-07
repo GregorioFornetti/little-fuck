6d7be8745587e321941f42b1efa07451b1ae4756
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-07-07 20:45:14 -0300

    ✨ feat: Historic Modal component

--- frontend/src/debug/components/HistoricModal.vue
+++ frontend/src/debug/components/HistoricModal.vue
@@ -1,8 +1,9 @@
 
 <script setup lang="ts">
-// Remover comentário abaixo quando implementar o componente, juntamente com esse comentário atual
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { ref } from 'vue';
 import BaseModal from '@/components/modal/BaseModal.vue';
+import { eventsHistoryList } from '../globals';
+import JSONViewer from './JSONViewer.vue';
 
 defineProps<{
   title: string,
@@ -11,4 +12,52 @@ defineProps<{
   jsonList: object[],
   closeModal?: () => void,
 }>();
+
+const index = ref(0);
 </script>
+
+<template>
+  <BaseModal
+    :show="show"
+    :title="title"
+    :close-modal="closeModal"
+  >
+    <template #body>
+      <label class="text-white font-bold">Nome do evento: </label>
+      <span class="text-white">{{ eventsHistoryList.length > 0 ? eventsHistoryList[index].event : "" }} </span>
+      <br>
+      <span class="text-white font-bold">{{ jsonLabel }}: </span>
+      <div class="h-96 !mt-0">
+        <JSONViewer :json="jsonList.length > 0 ? jsonList[index] : {}" />
+      </div>
+    </template>
+    <template #footer>
+      <div class="flex justify-between w-full">
+        <button
+          class="bg-blue-600 hover:bg-blue-800 text-white font-bold py-1 rounded w-40 disabled:opacity-50 disabled:hover:bg-blue-600"
+          :disabled="index === 0"
+          @click="index = Math.max(0, index - 1)"
+        >
+          Anterior
+        </button>
+        <div>
+          <input
+            v-model="index"
+            class="bg-neutral-800 text-white font-bold py-1 rounded w-40 text-right w-14"
+            type="number"
+            :min="0"
+            :max="Math.max(0, eventsHistoryList.length - 1)"
+          >
+          <span class="text-white"> / {{ Math.max(0, eventsHistoryList.length - 1) }}</span>
+        </div>
+        <button
+          class="bg-blue-600 hover:bg-blue-800 text-white font-bold py-1 rounded w-40 disabled:opacity-50 disabled:hover:bg-blue-600"
+          :disabled="index === Math.max(0, eventsHistoryList.length - 1)"
+          @click="index = Math.min(Math.max(0, eventsHistoryList.length - 1), index + 1)"
+        >
+          Próximo
+        </button>
+      </div>
+    </template>
+  </BaseModal>
+</template>
