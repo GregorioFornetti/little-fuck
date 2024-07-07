2d550550c56aba662eaa3c852be8356eeb0c694c
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-07-07 01:13:24 -0300

    🔧 chore: Add debug messages to backend

--- backend/src/debug/addDebugEmitAfterEachEmit.ts
+++ backend/src/debug/addDebugEmitAfterEachEmit.ts
@@ -13,8 +13,16 @@ export function addDebugEmitAfterEachEmit(io: Server) {
         const player = createPlayer(io, socket.id);
         originalEmit.call(this, 'debug', player.lobby);
       }
+
       return true;
     };
+
+    socket.onAny((event, ...args) => {
+      console.log(`Received event: ${event}`);
+      console.log(`From player with id: ${socket.id}`);
+      console.log(`With the following arguments: ${JSON.stringify(args)}\n`);
+    });
+
     next();
   });
 }
