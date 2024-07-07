017001264333d8ed3f347142bf8c1b6ce793f350
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-07-07 01:15:52 -0300

    ✨ feat: CommandsContainer component

--- frontend/src/debug/components/CommandsContainer.vue
+++
@@ -1,7 +0,0 @@
-
-<script setup lang="ts">
-// Remover comentário abaixo quando implementar o componente, juntamente com esse comentário atual
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { eventsEmitter } from '@/connection';
-
-</script>
