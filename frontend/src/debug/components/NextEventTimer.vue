8d3a9142b73fd37cf682f9e3d9f04e326e6f6a33
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-07-08 22:18:03 -0300

    ✨ feat: Next event timer component

--- frontend/src/debug/components/NextEventTimer.vue
+++ frontend/src/debug/components/NextEventTimer.vue
@@ -1,5 +1,6 @@
 
 <script setup lang="ts">
+import clockImage from '@/assets/clock.png';
 
 defineProps<{
   nextEventTime: number,
@@ -7,3 +8,48 @@ defineProps<{
 }>();
 
 </script>
+
+<template>
+  <div class="component-container">
+    <div class="clock-container">
+      <img
+        :src="clockImage"
+        alt="Clock"
+        class="clock-image"
+      >
+      <div class="clock-number">
+        {{ nextEventTime }}
+      </div>
+    </div>
+    <p class="text-lg text-center">
+      {{ nextEventName }}
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.component-container {
+  width: 100px;
+}
+
+.clock-container {
+  position: relative;
+  width: 100%;
+}
+
+.clock-image {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+
+.clock-number {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: black;
+  font-size: 32px;
+  font-weight: bold;
+}
+</style>
