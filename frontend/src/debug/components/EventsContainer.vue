3efdb6ecfc7c53cb8e33ae0e0e79ec7313bbe294
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-07-14 00:58:11 -0300

    ✨ feat: Events container component

--- frontend/src/debug/components/EventsContainer.vue
+++ frontend/src/debug/components/EventsContainer.vue
@@ -1,10 +1,42 @@
 
 <script setup lang="ts">
-// Remover comentário abaixo quando implementar o componente, juntamente com esse comentário atual
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { ref } from 'vue';
 import HistoricModal from './HistoricModal.vue';
-
-// Remover comentário abaixo quando implementar o componente, juntamente com esse comentário atual
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { eventsHistoryList } from '../globals';
+import type { EventLog } from '../globals';
+
+const showHistoricModal = ref(false);
 </script>
+
+<template>
+  <div class="w-full h-full border-solid border-2 rounded-xl flex flex-col">
+    <div class="border-solid border-b-2 text-center font-bold p-2">
+      Histórico de eventos
+    </div>
+    <div class="grow overflow-y-scroll">
+      <p
+        v-for="(eventLog, index) in eventsHistoryList.slice().reverse()"
+        :key="index"
+        class="ml-4 mt-2 truncate"
+      >
+        {{ eventLog.event }}
+      </p>
+    </div>
+    <div class="border-solid border-t-2">
+      <button
+        class="bg-blue-600 hover:bg-blue-800 text-white font-bold py-1 rounded-b-xl w-full"
+        @click="showHistoricModal = true"
+      >
+        Histórico completo
+      </button>
+    </div>
+  </div>
+
+  <HistoricModal
+    title="Histórico de eventos"
+    :show="showHistoricModal"
+    json-label="Parâmetros"
+    :json-list="eventsHistoryList.map((event: EventLog) => event.params)"
+    :close-modal="() => showHistoricModal = false"
+  />
+</template>
