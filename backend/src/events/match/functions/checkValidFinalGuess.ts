39756e51d6cb108ba5ed085a67d44a40875ff0d8
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-08-29 13:17:05 -0300

    ✨ feat: Implement checkValidFinalGuess function

--- backend/src/events/match/functions/checkValidFinalGuess.ts
+++ backend/src/events/match/functions/checkValidFinalGuess.ts
@@ -1,4 +1,7 @@
+
 import Lobby from '../../../interfaces/Lobby';
+import i18n from '../../../plugins/i18n';
+import { getNextPlayerId } from '../../functions/getNextPlayerId';
 
 /**
  *  Irá retornar `false` caso seja o último jogador a palpitar e o palpite irá fazer com que o somatório de todos palpite
@@ -11,5 +14,30 @@ import Lobby from '../../../interfaces/Lobby';
 // Remover comentário abaixo quando implementar a função, juntamente com esse comentário atual
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function checkValidFinalGuess(lobby: Lobby, currentGuess: number): boolean {
+  if (!lobby.game) {
+    throw new Error(i18n.t('COMMON.ERROR.NOT_IN_GAME'));
+  }
 
+  if (!lobby.game.match) {
+    throw new Error(i18n.t('COMMON.ERROR.NOT_IN_MATCH'));
+  }
+
+  if (!lobby.game.match.nextPlayerId) {
+    throw new Error(i18n.t('COMMON.ERROR.NO_NEXT_PLAYER_IN_MATCH'));
+  }
+
+  const currentPlayerId = lobby.game.match.nextPlayerId;
+  const nextPlayerId = getNextPlayerId(currentPlayerId, lobby);
+  const isCurrentPlayerLast = nextPlayerId === lobby.game.match.roundFirstPlayerId;
+
+  let totalWinGuesses = 0;
+  for (const playerId in lobby.game.match.players) {
+    if (lobby.game.match.players[playerId].numWinsNeeded) {
+      totalWinGuesses += lobby.game.match.players[playerId].numWinsNeeded;
+    }
+  }
+  totalWinGuesses += currentGuess;
+  const isGuessesSumEqualToCurrentNumCards = totalWinGuesses === lobby.game.numRounds;
+
+  return isCurrentPlayerLast && isGuessesSumEqualToCurrentNumCards;
 }
