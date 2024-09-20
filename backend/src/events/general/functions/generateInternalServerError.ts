7fb900c0caf11f278999fb5a6dd79f39e1e6f4a4
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-09-20 14:45:49 -0300

    🐛 fix: GenerateInternalServerError
    - Não é possível fazer chamadas com o player como parâmetro (apenas com o lobby). Isso por que  o Player não vai necessariamente estar instanciado no momento. Faz mais sentido chamar com o lobby.

--- backend/src/events/general/functions/generateInternalServerError.ts
+++ backend/src/events/general/functions/generateInternalServerError.ts
@@ -1,16 +1,16 @@
 
-import Player from '../../../interfaces/Player';
+import Lobby from '../../../interfaces/Lobby';
 
 /**
  *  Essa função deve ser chamada quando ocorrer um erro inesperado no servidor. Essa função irá informar os clientes da ocorrência
  *  do erro, finalizando o lobby de todos os jogadores (para não ocorrer erros piores, podendo "crashar" o servidor). Também
  *  registra o erro no console do servidor.
  *
- *  @param player Player que será usado para emitir o evento de erro para o(s) cliente(s).
+ *  @param lobby Lobby que ocorreu o erro.
  *  @param error Erro que será registrado no console do servidor.
  */
 // Remover comentário abaixo quando implementar a função, juntamente com esse comentário atual
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function generateInternalServerError(player: Player, error: Error): void {
+export function generateInternalServerError(lobby: Lobby, error: Error): void {
 
 }
