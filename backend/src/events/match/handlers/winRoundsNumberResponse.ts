f5f4e48fe79374d38163a94e6e72e35b74b9a501
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-10-06 16:34:43 -0300

    ✨ feat: Implement handleWinRoundsNumberResponse

--- backend/src/events/match/handlers/winRoundsNumberResponse.ts
+++ backend/src/events/match/handlers/winRoundsNumberResponse.ts
@@ -1,4 +1,12 @@
+import Timer from 'easytimer.js';
 import Player from '../../../interfaces/Player';
+import { getNextPlayerId } from '../../functions/getNextPlayerId';
+import { generateInternalServerError } from '../../general/functions/generateInternalServerError';
+import { checkValidFinalGuess } from '../functions/checkValidFinalGuess';
+import { generateAutomaticNumWinResponse } from '../functions/generateAutomaticNumWinResponse';
+import Lobby from '../../../interfaces/Lobby';
+import { endSpecialMatch } from '../functions/endSpecialMatch';
+import { startNewRound } from '../../round/functions/startNewRound';
 
 /**
  *  Mensagem que o cliente enviará para indicar o seu palpite.
@@ -18,8 +26,75 @@ import Player from '../../../interfaces/Player';
  *  @param player Objeto contendo informações do jogador que acaba de chamar o evento
  *  @param numWinsRounds Número de rodadas que o jogador acha que irá vencer
  */
-// Remover comentário abaixo quando implementar a função, juntamente com esse comentário atual
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function handleWinRoundsNumberResponse(player: Player, numWinsRounds: number) {
+  if (!player.lobby) {
+    player.eventsEmitter.Match.emitWinRoundsNumberError('not-in-lobby');
+    return;
+  }
+  const lobby: Lobby = player.lobby;
 
+  if (!lobby.game) {
+    player.eventsEmitter.Match.emitWinRoundsNumberError('not-in-game');
+    return;
+  }
+
+  if (!lobby.game.match) {
+    player.eventsEmitter.Match.emitWinRoundsNumberError('not-in-match');
+    return;
+  }
+
+  if (!(player.playerId === lobby.game.match.nextPlayerId)) {
+    player.eventsEmitter.Match.emitWinRoundsNumberError('not-your-turn');
+    return;
+  }
+
+  if (numWinsRounds < 0) {
+    player.eventsEmitter.Match.emitWinRoundsNumberError('negative-is-invalid');
+    return;
+  }
+
+  try {
+    if (!checkValidFinalGuess(player.lobby, numWinsRounds)) {
+      player.eventsEmitter.Match.emitWinRoundsNumberError('num-wins-equals-num-cards');
+      return;
+    }
+
+    lobby.game.timer.stop();
+    lobby.game.timer = new Timer();
+
+    lobby.game.match.players[player.playerId].numWinsNeeded = numWinsRounds;
+
+    lobby.game.match.nextPlayerId = getNextPlayerId(lobby.game.match.nextPlayerId, lobby);
+
+    if (lobby.game.match.nextPlayerId === lobby.game.currentPlayerId) {
+      // O último jogador acabou de palpitar, hora de começar uma rodada ou finalizar a partida especial
+      lobby.game.match.nextPlayerId = undefined;
+      if (lobby.game.numRounds === 1) {
+        // Se tivermos apenas uma carta, então é uma partida especial. Nesse caso, precisamos finalizar a partida especial
+        lobby.game.status = 'ending_special_match';
+        lobby.game.timer.start({ countdown: true, startValues: { seconds: 5 } });
+        lobby.game.timer.addEventListener('targetAchieved', () => {
+          endSpecialMatch(lobby);
+        });
+      } else {
+        // Se tivermos mais de uma carta, então é uma partida normal. Nesse caso, precisamos começar uma rodada
+        lobby.game.status = 'starting_round';
+        lobby.game.timer.start({ countdown: true, startValues: { seconds: 5 } });
+        lobby.game.timer.addEventListener('targetAchieved', () => {
+          startNewRound(lobby);
+        });
+      }
+    } else {
+      // Ainda restam palpites a serem feitos... É preciso cadastrar o timer para palpite automático do próximo player
+      lobby.game.timer.start({ countdown: true, startValues: { seconds: 15 } });
+      lobby.game.timer.addEventListener('targetAchieved', () => {
+        generateAutomaticNumWinResponse(lobby);
+      });
+    }
+
+    player.eventsEmitter.Match.emitWinRoundsNumberUpdate(numWinsRounds, lobby.game.match.nextPlayerId);
+
+  } catch (error) {
+    generateInternalServerError(lobby, error as Error);
+  }
 }
